c3eaa78cecbe9f870e203a234634a539017230bb
Andrew <Andrew@Werdna.attlocal.net>
2023-02-20 15:17:44 -0600

    First commit

---
+++ AoAProj1/src/Points.java
@@ -0,0 +1,36 @@
+
+public class Points {
+	double xCord;
+	double yCord;
+	public Points(double x, double y) {
+		this.xCord	= this.yCord;
+	}
+	//todo: add functions to solve alg
+	/**
+	 * @return the xCord
+	 */
+	public double getxCord() {
+		return xCord;
+	}
+	/**
+	 * @param xCord the xCord to set
+	 */
+	public void setxCord(double xCord) {
+		this.xCord = xCord;
+	}
+	/**
+	 * @return the yCord
+	 */
+	public double getyCord() {
+		return yCord;
+	}
+	/**
+	 * @param yCord the yCord to set
+	 */
+	public void setyCord(double yCord) {
+		this.yCord = yCord;
+	}
+	
+	
+	
+}
