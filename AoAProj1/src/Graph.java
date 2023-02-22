97a1bb1db367dfed646de0a793b90858f12e34dc
Andrew <Andrew@Werdna.attlocal.net>
2023-02-22 15:36:26 -0600

    Added upper tan function

--- AoAProj1/src/Graph.java
+++ AoAProj1/src/Graph.java
@@ -69,6 +69,7 @@ public class Graph {
 	public ArrayList<Line> combineSides( ArrayList<Line> leftSide, ArrayList<Line> rightSide )
 	{
 		Line lT = lowerTangent(leftSide,rightSide);
+		Line uT = upperTangent(leftSide,rightSide);
 		return null;
 	}
 	
@@ -85,8 +86,6 @@ public class Graph {
 		Points b = rightSide.get(c).point1; /* Leftmost point of b */
 		Line lT = new Line(a,b);
 		System.out.println("Start: "+lT + "\t" + lT.getSlope() + "\t"+lT.getyIntercept());
-		System.out.println(isLowerTangent(leftSide,lT));
-		System.out.println(isLowerTangent(rightSide,lT));
 		while(isLowerTangent(rightSide,lT)==false || isLowerTangent(leftSide,lT)==false ) {
 			while(isLowerTangent(leftSide,lT)==false) {
 				if(i == 0)
@@ -97,7 +96,6 @@ public class Graph {
 				lT = new Line(a,b);
 			}
 			while(isLowerTangent(rightSide,lT)==false) {
-				System.out.println("test1");
 				if(c == leftSide.size()-1)
 					c=0;
 				else
@@ -113,12 +111,57 @@ public class Graph {
 	public boolean isLowerTangent( ArrayList<Line> shape, Line tan) {
 		int i = 0;
 		Points p=shape.get(i).getPoint1();
-		for(i=0;i<shape.size()-1;i++) {
+		for(i=0;i<shape.size();i++) {
 			p=shape.get(i).getPoint1();
 			if(p != tan.getPoint1()&& p!=tan.getPoint2()&&pointAboveLine(tan,p)==false)
 				return false;
 		}
 		return true;
+	}
+	public Line upperTangent( ArrayList<Line> leftSide, ArrayList<Line> rightSide )
+	{
+		int i =0;
+		Points a = leftSide.get(0).point1;
+		for(i = 0; i< leftSide.size()-1;i++) {
+			if(leftSide.get(i).getPoint1().getxCord()>a.getxCord())
+				a=leftSide.get(i).getPoint1();
+		} /* Rightmost point of a */
+		int c = 0;
+		Points b = rightSide.get(c).point1; /* Leftmost point of b */
+		Line lT = new Line(a,b);
+		System.out.println("Upper Start: "+lT + "\t" + lT.getSlope() + "\t"+lT.getyIntercept());
+		while(isUpperTangent(rightSide,lT)==false || isUpperTangent(leftSide,lT)==false ) {
+			while(isUpperTangent(leftSide,lT)==false) {
+				if(i == 0)
+					i=leftSide.size()-1;
+				else
+					i--;
+				a=leftSide.get(i).getPoint1();
+				lT = new Line(a,b);
+			}
+			while(isUpperTangent(rightSide,lT)==false) {
+				if(c == leftSide.size()-1)
+					c=0;
+				else
+					c++;
+				b = rightSide.get(c).getPoint1();
+				lT = new Line(a,b);
+			}
+		}
+		System.out.println("Upper Final: "+lT);
+		
+		return lT;
+	}
+	public boolean isUpperTangent( ArrayList<Line> shape, Line tan) {
+		int i = 0;
+		Points p=shape.get(i).getPoint1();
+		for(i=0;i<shape.size();i++) {
+			p=shape.get(i).getPoint1();
+			if(p != tan.getPoint1()&& p!=tan.getPoint2()&&pointAboveLine(tan,p)==true)
+				return false;
+		}
+		return true;
+		
 	}
 	public boolean pointAboveLine(Line l, Points p) {
 		double lineY = l.getSlope()*p.getxCord()+l.getyIntercept();
